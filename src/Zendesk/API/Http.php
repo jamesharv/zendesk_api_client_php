b4347717ed2d05145d6516226690c3f28ed58a94
Jose Consador <joseconsador@gmail.com>
2015-06-15 16:55:15 +0800

    Remove var_dump(), pass Guzzle exception to our own exception class

--- src/Zendesk/API/Http.php
+++ src/Zendesk/API/Http.php
@@ -2,8 +2,11 @@
 
 namespace Zendesk\API;
 
+use Zendesk\API\Exceptions\ResponseException;
+
 /**
  * HTTP functions via curl
+ *
  * @package Zendesk\API
  */
 class Http
@@ -14,14 +17,14 @@ class Http
      * Prepares an endpoint URL with optional side-loading
      *
      * @param string $endPoint
-     * @param array $sideload
-     * @param array $iterators
+     * @param array  $sideload
+     * @param array  $iterators
      *
      * @return string
      */
     public static function prepareQueryParams(array $sideload = null, array $iterators = null)
     {
-        $addParams = array();
+        $addParams = [];
         // First look for side-loaded variables
         if (is_array($sideload)) {
             $addParams['include'] = implode(',', $sideload);
@@ -30,7 +33,7 @@ class Http
         // Next look for special collection iterators
         if (is_array($iterators)) {
             foreach ($iterators as $k => $v) {
-                if (in_array($k, array('per_page', 'page', 'sort_order', 'sort_by'))) {
+                if (in_array($k, ['per_page', 'page', 'sort_order', 'sort_by'])) {
                     $addParams[$k] = $v;
                 }
             }
@@ -43,13 +46,13 @@ class Http
      * Use the send method to call every endpoint except for oauth/tokens
      *
      * @param HttpClient $client
-     * @param string $endPoint E.g. "/tickets.json"
-     * @param array $options
-     *          Available options are listed below:
-     *          array $queryParams Array of unencoded key-value pairs, e.g. ["ids" => "1,2,3,4"]
-     *          array $postFields Array of unencoded key-value pairs, e.g. ["filename" => "blah.png"]
-     *          string $method "GET", "POST", etc. Default is GET.
-     *          string $contentType Default is "application/json"
+     * @param string     $endPoint E.g. "/tickets.json"
+     * @param array      $options
+     *                             Available options are listed below:
+     *                             array $queryParams Array of unencoded key-value pairs, e.g. ["ids" => "1,2,3,4"]
+     *                             array $postFields Array of unencoded key-value pairs, e.g. ["filename" => "blah.png"]
+     *                             string $method "GET", "POST", etc. Default is GET.
+     *                             string $contentType Default is "application/json"
      *
      * @return array The response body, parsed from JSON into an associative array
      */
@@ -60,9 +63,9 @@ class Http
     ) {
         $options = array_merge(
             [
-                'method' => 'GET',
+                'method'      => 'GET',
                 'contentType' => 'application/json',
-                'postFields' => [],
+                'postFields'  => [],
                 'queryParams' => []
             ],
             $options
@@ -79,10 +82,10 @@ class Http
             $method,
             $url,
             [
-                'query' => $queryParams,
-                'body' => json_encode($postFields),
+                'query'   => $queryParams,
+                'body'    => json_encode($postFields),
                 'headers' => [
-                    'Accept' => 'application/json',
+                    'Accept'       => 'application/json',
                     'Content-Type' => $contentType
                 ]
             ]
@@ -90,31 +93,29 @@ class Http
 
         try {
             $response = $client->guzzle->send($request);
+
+            $responseCode = $response->getStatusCode();
+
+            $client->setDebug(
+                $response->getHeaders(),
+                $responseCode,
+                10,
+                null
+            );
         } catch (\GuzzleHttp\Exception\ClientException $e) {
-            var_dump($e->getRequest()->getUri());
-            var_dump($e->getResponse()->getStatusCode());
+            throw new ResponseException($endPoint, null, null, $e);
         }
 
-        $responseCode = $response->getStatusCode();
-        $parsedResponseBody = json_decode($response->getBody());
-
-        $client->setDebug(
-            $response->getHeaders(),
-            $responseCode,
-            10,
-            null
-        );
-
-        return $parsedResponseBody;
+        return json_decode($response->getBody());
     }
 
     /**
      * Specific case for OAuth. Run /oauth.php via your browser to get an access token
      *
      * @param HttpClient $client
-     * @param string $code
-     * @param string $oAuthId
-     * @param string $oAuthSecret
+     * @param string     $code
+     * @param string     $oAuthId
+     * @param string     $oAuthSecret
      *
      * @throws \Exception
      *
@@ -123,20 +124,19 @@ class Http
     public static function oauth(HttpClient $client, $code, $oAuthId, $oAuthSecret)
     {
         $url = 'https://' . $client->getSubdomain() . '.zendesk.com/oauth/tokens';
-        $method = 'POST';
 
         $curl = (isset(self::$curl)) ? self::$curl : new CurlRequest;
         $curl->setopt(CURLOPT_URL, $url);
         $curl->setopt(CURLOPT_POST, true);
-        $curl->setopt(CURLOPT_POSTFIELDS, json_encode(array(
-            'grant_type' => 'authorization_code',
-            'code' => $code,
-            'client_id' => $oAuthId,
+        $curl->setopt(CURLOPT_POSTFIELDS, json_encode([
+            'grant_type'    => 'authorization_code',
+            'code'          => $code,
+            'client_id'     => $oAuthId,
             'client_secret' => $oAuthSecret,
-            'redirect_uri' => ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-            'scope' => 'read'
-        )));
-        $curl->setopt(CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            'redirect_uri'  => ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+            'scope'         => 'read'
+        ]));
+        $curl->setopt(CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
         $curl->setopt(CURLINFO_HEADER_OUT, true);
         $curl->setopt(CURLOPT_RETURNTRANSFER, true);
         $curl->setopt(CURLOPT_CONNECTTIMEOUT, 30);
